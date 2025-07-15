5e959d85b153d637a44ef5f9fa50291ca4724b5f
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2025-07-15 18:03:53 +0530

    video detalis got from fetching api

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { use } from 'react'
 import SideBar from './SideBar'
 import Thumbnail from './Thumbnail'
 
 
 function Home() {
+const [videos,setVideos]=React.useState([]);
+const api_key='AIzaSyChybMFGahJyBPOveab8QHXw9m9MMVZ7-w';
+const api_url=`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=20&regionCode=IN&key=${api_key}`;
+React.useEffect(() => {
+
+   const fetchVideos=async ()=>{
+    const res=await fetch(api_url)
+    const data=await res.json();
+    setVideos(data.items);
+   }
+   fetchVideos();
+
+},[]);
+console.log(videos);
+    
   return (
     <div className='flex'>
         
@@ -19,3 +34,8 @@ function Home() {
 }
 
 export default Home
+
+
+
+//AIzaSyChybMFGahJyBPOveab8QHXw9m9MMVZ7-w
+//https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=20&regionCode=IN&key=YOUR_API_KEY
